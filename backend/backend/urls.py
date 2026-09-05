a3784d523f56e2cac6f697a0b1a8e512de49f23d
UnicornSereJa <sergovan2005@gmail.com>
2026-09-05 19:42:44 +0300

    FIX27: Relaxdev

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenRefreshView
 from api.serializers_jwt import CustomTokenObtainPairView
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Отдаём логотипы из папки frontend/public/images
+    path('images/<path:path>', serve, {'document_root': os.path.join(settings.BASE_DIR, 'frontend/public/images')}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Все остальные маршруты отдаём React (SPA)

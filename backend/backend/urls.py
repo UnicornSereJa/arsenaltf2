7cef9b932d5cec855b8ae1038622410d744e9f21
UnicornSereJa <sergovan2005@gmail.com>
2026-09-05 17:47:17 +0300

    FIX21: Relaxdev

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenRefreshView
 from api.serializers_jwt import CustomTokenObtainPairView
 
-# Простая заглушка для корневого маршрута
-def home(request):
-    return HttpResponse("Арсенал TF2 — сервер работает! 🚀")
-
 urlpatterns = [
-    path('', home, name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
